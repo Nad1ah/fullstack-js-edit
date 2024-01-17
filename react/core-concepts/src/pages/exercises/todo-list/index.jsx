711f289fbfc250a5a00b5e78fefdd44e49f0ddbf
PalmsDev <97535820+Lokito2845@users.noreply.github.com>
2024-01-17 21:22:40 +0000

    exercicios

--- react/core-concepts/src/pages/exercises/todo-list/index.jsx
+++ react/core-concepts/src/pages/exercises/todo-list/index.jsx
@@ -1,29 +1,42 @@
 import { useState } from "react";
-function TodoListPage()
 
-{
-    const [inputValue, setInputValue] = useState("");
-    const handleSubmit=(event) => 
-    console.log(event);
-const [itens, setInputItens] = useState("")
-    return (
+function TodoListPage() {
+  const [inputValue, setInputValue] = useState("");
+
+  const [itens, setInputItens] = useState([]);
+
+  const handleRemove = (index) => {
+    const newArr = [...itens];
+    const arr = newArr.filter((item, key) => index !== key);
+    setInputItens(arr);
+  };
+
+  return (
     <div>
-        <h1>Todo List</h1>
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={inputValue} onChange={(event)=>setInputValue(event.target.value)} />
-                <button >Adicionar</button>
-            </form>
-        </div>
-        <div>
-            <ul>
-                {itens.map((item)=>(
-                    <li key={item}>(item)</li>
-                ))}
-            </ul>
-        </div>
+      <h1>Todo List</h1>
+      <div>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            setInputItens([...itens, inputValue]);
+          }}
+        >
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+        <ul>
+          {itens.map((item, key) => (
+            <li key={`${key}-${item}`}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <div></div>
     </div>
-    );
+  );
 }
 
-export default TodoListPage
+export default TodoListPage;
